aef8d513e1bf485488dd51f656cfc8ffe29eba52
Lamarana Diallo <lamdiallo@gmail.com>
2022-07-07 23:01:52 -0400

    ajout de ibm mq

--- src/main/java/com/ld/springcloudrabbitmq/SpringCloudRabbitmqApplication.java
+++ src/main/java/com/ld/springcloudrabbitmq/SpringCloudRabbitmqApplication.java
@@ -6,8 +6,10 @@ import java.util.function.Function;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class SpringCloudRabbitmqApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ld/springcloudrabbitmq/controller/MyRestControlller.java
+++ src/main/java/com/ld/springcloudrabbitmq/controller/MyRestControlller.java
@@ -1,10 +1,15 @@
 package com.ld.springcloudrabbitmq.controller;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +30,9 @@ public class MyRestControlller {
 	private String routingKey2;
 
 	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
 	@GetMapping(value = "/api", produces = MediaType.TEXT_PLAIN_VALUE)
@@ -32,9 +40,16 @@ public class MyRestControlller {
 
 		// System.out.println("Message REST recu : " + message);
 		System.out.println("#############################################################################3");
+
 		String str = String.join("", "Voici le message : ", message);
 
-		// fournisseur.apply(str);
+		sendToIbmMq(str);
+
+		return str;
+	}
+
+	private void sendToRabbitMq(String message, String str) {
+
 		if (StringUtils.length(message) <= 5) {
 			rabbitTemplate.convertAndSend(exchange, routingKey1, str);
 		} else if (StringUtils.length(message) <= 10) {
@@ -42,8 +57,19 @@ public class MyRestControlller {
 		} else {
 			System.out.println("Trop long");
 		}
+	}
 
-		return str;
+	private void sendToIbmMq(String str) {
+		jmsTemplate.convertAndSend("DEV.QUEUE.1", str);
+	}
+
+	// @JmsListener(destination = "DEV.QUEUE.1")
+	public void getMessageFromIbmMq(final Message message) throws JMSException {
+
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			System.out.println("Message JMS recu : " + textMessage.getText());
+		}
 	}
 
 }
